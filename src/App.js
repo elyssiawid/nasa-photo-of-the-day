dc353e7ece31f2209a8c49a06fd66ae27d4b4e34
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-17 13:59:31 -0700

    added NASAGrid.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import NASAGrid from "./Components/NASAGrid";
+
+const [date, setDate] = useState();
+const [url, setURL] = useState();
+useEffect(() => {
+  Axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(res => {
+      setMediaURL(res.data.url), setDate(res.data.date);
+    })
+    .catch(err => console.log(err));
+}, []);
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Photo of the Day</h1>
+      <section>
+        <b> Build a NASA card here </b>
+      </section>
     </div>
   );
 }
