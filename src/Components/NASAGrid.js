77ef15493342a57a657c341998e8b7d6a5dd3ab6
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-17 17:58:53 -0700

    linked NASA card to nasa grid

--- src/Components/NASAGrid.js
+++ src/Components/NASAGrid.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import NASACard from "./NASACard";
 import axios from "axios";
 
 function NASAGrid(props) {
   console.log("nasagridprops", props.nasaData);
-
+  const cards = props.nasaData.map(x => <NASACard />);
   return <div>Grid</div>;
 }
 
 export default NASAGrid;
+
+const map1 = array1.map(x => x * 2);
